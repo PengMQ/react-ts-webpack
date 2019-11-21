cb720e34bd395b0241f577d0aa7e241949ef201b
彭梦秋 <pengmengqiu@pengmengqius-MacBook-Pro.local>
2019-11-21 21:16:04 +0800

    [mengqiu] edit webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     devtool: "source-map",
     resolve: {
@@ -28,5 +29,10 @@ module.exports = {
                 loader: 'source-map-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
 }
