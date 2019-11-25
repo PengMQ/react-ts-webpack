564ffa209df4452c31410193060541bb21f30f97
彭梦秋 <pengmengqiu@pengmengqius-MacBook-Pro.local>
2019-11-25 19:04:32 +0800

    [mengqiu] load images in tsx

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,13 @@ module.exports = {
             {
                 test: /\.scss/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
         ]
     },
     plugins: [
