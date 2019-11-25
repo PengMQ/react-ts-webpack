6863e8222e26891f632a7ce1411ce4f1d337e58f
彭梦秋 <pengmengqiu@pengmengqius-MacBook-Pro.local>
2019-11-25 21:57:21 +0800

    [mengqiu] extract css from js when build

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,68 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-module.exports = {
-    devtool: "source-map",
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.scss']
-    },
-    entry: {
-        main: './src/index.tsx'
-    },
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: "[name].[contenthash].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_moduls/,
-                use: [
-                    {
-                        loader: "awesome-typescript-loader"
-                    }
-                ]
-            },
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: 'source-map-loader'
-            },
-            {
-                test: /\.scss/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
-        new CleanWebpackPlugin()
-    ],
-    optimization: {
-        splitChunks: {
-            chunks: "all"
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = (env, argv)=>{
+    const isDevMode = argv.mode === 'development';
+    return {
+        devtool: "source-map",
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.scss']
+        },
+        entry: {
+            main: './src/index.tsx'
+        },
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: isDevMode ? "[name].[hash].js" : "[name].[contenthash].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts(x?)$/,
+                    exclude: /node_moduls/,
+                    use: [
+                        {
+                            loader: "awesome-typescript-loader"
+                        }
+                    ]
+                },
+                {
+                    enforce: "pre",
+                    test: /\.js$/,
+                    loader: 'source-map-loader'
+                },
+                {
+                    test: /\.scss/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        'css-loader',
+                        'sass-loader']
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        'file-loader',
+                    ],
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: isDevMode ? '[name].css' : '[name].[contenthash].css',
+                chunkFilename: isDevMode ? '[id].css' : '[id].[contenthash].css'
+            })
+        ],
+        optimization: {
+            splitChunks: {
+                chunks: "all"
+            }
         }
     }
 }
