b867d8408f03a580da8d45e9acc98f178032be89
彭梦秋 <pengmengqiu@pengmengqius-MacBook-Pro.local>
2019-12-04 11:40:39 +0800

    [mengqiu] successfully config imgs mock for Jest

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {shallow} from "enzyme";
 
 import App from './App';
-test('There is an Image in the App component', () => {
-    const result = shallow(<App />).contains(<h1>hehe</h1>);
+test('The title should contain hooks', () => {
+    const result = shallow(<App />).contains(<h6>React hooks example</h6>);
     expect(result).toBeTruthy();
 });
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,24 +2,22 @@ import * as React from 'react';
 import {useState} from "react";
 import './App.scss';
 
-// const Moon = require('./images/moon.jpg');
+const Moon = require('./images/moon.jpg');
 
 function App() {
     const [count, setCount] = useState(0);
-    // return (
-    //     <div>
-    //         <div>
-    //             <div className='black'>You have clicked
-    //                 <span className='count'>{count}</span>
-    //                 times.
-    //             </div>
-    //             <button className="green button" onClick={() => setCount(count + 1)}> click</button>
-    //         </div>
-    //         {/*<img src={Moon} alt=""/>*/}
-    //     </div>
-    // );
     return (
-       <h1>hehe</h1>
+        <div>
+            <h6>React hooks example</h6>
+            <div>
+                <div className='black'>You have clicked
+                    <span className='count'>{count}</span>
+                    times.
+                </div>
+                <button className="green button" onClick={() => setCount(count + 1)}> click</button>
+            </div>
+            <img src={Moon} alt=""/>
+        </div>
     );
 
 }
