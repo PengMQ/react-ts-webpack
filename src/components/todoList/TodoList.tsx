c410d931e51aa68093a2a8ceff0d0562b92830f1
彭梦秋 <pengmengqiu@pengmengqius-MacBook-Pro.local>
2019-12-09 18:59:04 +0800

    [mengqiu] refactoring TodoList to has React.FC type

--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -1,19 +1,18 @@
-
 import * as React from "react";
 import {todoListInterface} from '../../types/types'
 import {TodoItem} from '../todoItem/TodoItem'
 
-const TodoList = (props: todoListInterface)=>{
-    return (
+const TodoList: React.FC<todoListInterface> = ({todoList}) => (
         <ul>
-            {props.todoList.map((todoItem)=>(
+            {todoList.map((todoItem) => (
                 <li key={todoItem.id}>
                     <TodoItem id={todoItem.id} text={todoItem.text} completed={todoItem.completed}/>
                 </li>
 
             ))}
         </ul>
-    )
-};
+    );
 
-export default TodoList
+export default TodoList;
+
+
