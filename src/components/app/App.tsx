49b48e539361645b1811ef327c64799d4ac9ff1b
彭梦秋 <pengmengqiu@pengmengqius-MacBook-Pro.local>
2019-12-09 19:21:24 +0800

    [mengqiu] remove unrelated code from App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import {useState} from "react";
-import './App.scss';
 import TodoList from '../todoList/TodoList'
 import {todoItemInterface} from '../../types/types'
 
-const Moon = require('./images/moon.jpg');
-
 function App() {
-    const [count, setCount] = useState(0);
     const [todos] = useState<todoItemInterface[]>([
         {id: 0, text: 'clean house', completed: false},
         {id: 1, text: 'cook dinner', completed: false}
@@ -15,14 +11,6 @@ function App() {
     return (
         <div>
             <h6>React hooks example</h6>
-            <div>
-                <div className='black'>You have clicked
-                    <span className='count'>{count}</span>
-                    times.
-                </div>
-                <button className="green button" onClick={() => setCount(count + 1)}> click</button>
-            </div>
-            <img src={Moon} alt=""/>
             <TodoList todoList={todos} />
         </div>
     );
