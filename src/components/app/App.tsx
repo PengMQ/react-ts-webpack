cbbaa5462cd5be610ed70b98ff28185e3a58f90e
彭梦秋 <pengmengqiu@pengmengqius-MacBook-Pro.local>
2020-03-03 23:16:26 +0800

    [mengqiu] get the correct i18n work for text, date and time

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,24 +1,28 @@
 import * as React from 'react';
-import {IntlProvider} from "react-intl";
+import {IntlProvider, FormattedMessage} from "react-intl";
 
 const App = () => {
     const locale = 'en';
     let messages = {
         en: {
             greeting: 'Hello',
+            time: "The time is {t, time, short}.",
+            date: "The date is {d, date}."
         },
         zh: {
-            greeting: "你好"
+            greeting: "你好",
+            time: "现在的时间是 {t, time, short}.",
+            date: "今天是 {d, date}."
         }
     };
     return (
         <>
             <IntlProvider locale={locale} key={locale} messages={messages[locale]}>
-                <div>hello</div>
+                <div><FormattedMessage id="greeting"/></div>
+                <div><FormattedMessage id="date" values={{ d: Date.now() }} /></div>
+                <div><FormattedMessage id="time" values={{ t: Date.now() }} /></div>
             </IntlProvider>
         </>
-
-
     );
 
 };
